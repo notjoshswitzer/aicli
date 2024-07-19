87ccd9cd1f183b9bfc2965f6bcffe33f55c87bc3
notjoshswitzer <joshswitzer1999@gmail.com>
2024-07-19 11:42:00 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# OpenAI config
+OAI_KEY = 'sk-proj-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' # https://platform.openai.com/api-keys
+OAI_LLM = 'gpt-4o-mini'                                              # https://platform.openai.com/docs/models
+
+# Ollama config
+OLLAMA_URL = 'http://localhost:11434/'                               # Ollama URL is remote (Ensure your service is public)
+OLLAMA_LLM = 'deepseek-coder-v2:16b'                                 # Update this to your default LLM model
