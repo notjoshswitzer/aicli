bd1b3a6ab7f852dd30314e13171f4128dfa35012
notjoshswitzer <joshswitzer1999@gmail.com>
2024-07-19 11:47:08 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ OAI_KEY = 'sk-proj-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' # https://p
 OAI_LLM = 'gpt-4o-mini'                                              # https://platform.openai.com/docs/models
 
 # Ollama config
-OLLAMA_URL = 'http://localhost:11434/'                               # Ollama URL is remote (Ensure your service is public)
-OLLAMA_LLM = 'deepseek-coder-v2:16b'                                 # Update this to your default LLM model
+OLLAMA_URL = 'http://localhost:11434/'                               # Ollama API endpoint (Ensure your Ollama service is public)
+OLLAMA_LLM = 'deepseek-coder-v2:16b'                                 # https://ollama.com/library
