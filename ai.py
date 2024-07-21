569a8881876ea7f3f431b0a91845f45e4243e1af
notjoshswitzer <joshswitzer1999@gmail.com>
2024-07-21 12:14:54 -0400

    Update ai.py
    Added options
    Added default LLM (Flags override config option)

--- ai.py
+++ ai.py
@@ -45,15 +45,25 @@ def stream_api_response(chat_history, args):
 
     hist_output = ''
     
-    # OpenAI
     if args.G:
+        LLM = 'oai'
+    elif args.O:
+        LLM = 'ollama'
+    else:
+        LLM = config.DEFAULT_LLM
+
+    # OpenAI
+    if LLM == 'oai':
         payload['model'] = config.OAI_LLM
         headers = {"Authorization": "Bearer " + config.OAI_KEY}
         response = requests.post("https://api.openai.com/v1/chat/completions", stream=True, json=payload, headers=headers)
     # Ollama
-    else:
+    elif LLM == 'ollama':
         payload['model'] = config.OLLAMA_LLM
         response = requests.post(config.OLLAMA_URL + 'api/chat', stream=True, json=payload)
+    else:
+        print("Invalid LLM")
+        exit()
 
     # Print time/LLM
     if not args.x:
@@ -65,7 +75,7 @@ def stream_api_response(chat_history, args):
         for line in response.iter_lines():
             if line:
                 try:
-                    if args.G:
+                    if LLM == 'oai':
                         char = json.loads(line.decode("utf-8")[6:])['choices'][0]['delta']['content']
                     else:
                         char = json.loads(line.decode("utf-8"))['message']['content']
@@ -82,7 +92,7 @@ def stream_api_response(chat_history, args):
             for line in response.iter_lines():
                 if line:
                     try:
-                        if args.G:
+                        if LLM == 'oai':
                             char = json.loads(line.decode("utf-8")[6:])['choices'][0]['delta']['content']
                         else:
                             char = json.loads(line.decode("utf-8"))['message']['content']
@@ -99,6 +109,29 @@ def stream_api_response(chat_history, args):
 
     return hist_output
 
+def extract_command(output):
+    command = []
+    command_list = ['bash', 'sh']
+    outputs = output.split('```')
+    # Iterate through code blocks
+    for snip in outputs:
+        # Logic to determine language
+        for lang in command_list:
+            if snip[:len(lang)] == lang:
+                command += [snip[len(lang)+1:][:-1]]
+    return command
+
+def extract_code(output):
+    code = []
+    code_list = ['python', 'java', 'javascript', 'cpp', 'c', 'ruby', 'html', 'css', 'php', 'sql', 'go', 'rust', 'perl', 'typescript', 'lua']
+    outputs = output.split('```')
+    # Iterate through code blocks
+    for snip in outputs:
+        # Logic to determine language
+        for lang in code_list:
+            if snip[:len(lang)] == lang:
+                code += [snip[len(lang)+1:][:-1]]
+    return code
 
 def main():
     parser = argparse.ArgumentParser(description="AI Assistant using Ollama API")
@@ -108,21 +141,47 @@ def main():
     parser.add_argument("-O", action="store_true", help="Use Ollama")
     parser.add_argument("-G", action="store_true", help="Use OpenAI (GPT)")
 
-    # ARG formatting
+    # ARG Functions
+    parser.add_argument("-E", action="store_true", help="Extract command(s) from last output")
+    parser.add_argument("-C", action="store_true", help="Extract code from last output")
+
+    # ARG Formatting
     parser.add_argument("-l", action="store_true", help="Print the last output")
     parser.add_argument("-x", action="store_true", help="Remove formatting from output")
 
-    # ARG engineering prompts
+    # ARG Engineering prompts
+    parser.add_argument("--security-audit", action="store_true", help="Perform a security audit on the provided code")
     parser.add_argument("--extract-wisdom", action="store_true", help="Summarize and extract key insights")
-    parser.add_argument("--explain-code", action="store_true", help="Explain the provided")
+    parser.add_argument("--explain-code", action="store_true", help="Explain the provided code")
     parser.add_argument("--optimize-code", action="store_true", help="Suggest optimizations")
     parser.add_argument("--find-bugs", action="store_true", help="Analyze code for potential issues")
     parser.add_argument("--document", action="store_true", help="Generate documentation for code")
     parser.add_argument("--architect", action="store_true", help="Propose architecture for the described problem")
     parser.add_argument("--refactor", action="store_true", help="Suggest refactoring strategys for the provided code")
-    parser.add_argument("--security-audit", action="store_true", help="Perform a security audit on the provided code")
 
     args = parser.parse_args()
+
+    # Print previous response
+    if args.l or args.E or args.C:
+        # Open history file to print previous output from LLM
+        with open('.aicli_last', 'r') as f:
+            output = f.read()
+            # Determine if output is wrapped in code wrappers
+            if args.C:
+                code = extract_code(output)
+                for c in code:
+                    print(c)
+            elif args.E:
+                code = extract_command(output)
+                for c in code:
+                    print(c)
+            elif args.x:
+                print(output)
+            else:
+                # Print output w/ markdown using rich library
+                markdown = Markdown(output)
+                console.print(markdown)
+        exit()
 
     # Check if there's piped input
     if not sys.stdin.isatty():
@@ -132,7 +191,7 @@ def main():
 
     # Construct the query
     if piped_input:
-        query = f"<content>{piped_input}</content>\n\nQUERY: " + " ".join(args.query)
+        query = f"<content>{piped_input}</content>\n\n" + " ".join(args.query)
     else:
         query = " ".join(args.query)
 
@@ -149,9 +208,9 @@ def main():
     if OS:
         system_message += ["The user operating system is {0}.".format(OS)]
 
-    # Prompt AI to respond with markdown if the 
+    # Prompt AI to respond with markdown
     if not args.x:
-        system_message += ["Your response should be in markdown format. "]
+        system_message += ["Your response should be in markdown format."]
     
     # Generic prompts to curate output
     if args.extract_wisdom:
@@ -182,27 +241,11 @@ def main():
         for chat in chat_history:
             print('ROLE: {0:<8} CONTENT: {1}'.format(chat['role'], chat['content']))
 
-    # Print previous response
-    if args.l:
-        # Open history file to print previous output from LLM
-        with open('.aicli_last', 'r') as f:
-            output = f.read()
-            # Determine if output is wrapped in code wrappers
-            if args.x:
-                if '```' in output[:6] and '```' in output[-6:]:
-                    # Extract code inside code wrappers
-                    output = output.split('```')[1]
-                print(output)
-            else:
-                # Print output w/ markdown using rich library
-                markdown = Markdown(output)
-                console.print(markdown)
     # Call the selected API
-    else:
-        output = stream_api_response(chat_history, args)
-        # Write output to file in event -l is called for previous response
-        with open('.aicli_last', 'w') as f:
-            f.write(''.join(output))
+    output = stream_api_response(chat_history, args)
+    # Write output to file in event -l is called for previous response
+    with open('.aicli_last', 'w') as f:
+        f.write(''.join(output))
 
 if __name__ == "__main__":
     main()
